dfa6baea1c1df641bf61544f4cea575069baac59
SmitBorsadiya <borsadiya5525smit@gmail.com>
2024-10-15 21:16:16 +0530

    feat(authentication): Worked on authentication using clerk

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 const Navbar = () => {
@@ -27,13 +28,8 @@ const Navbar = () => {
           <span className="text-xs leading-3 font-medium">Smit</span>
           <span className="text-[10px] text-gray-500 text-right">Admin</span>
         </div>
-        <Image
-          src="/avatar.png"
-          alt=""
-          width={36}
-          height={36}
-          className="rounded-full"
-        />
+        {/* <Image src="/avatar.png" alt="" width={36} height={36} className="rounded-full" /> */}
+        <UserButton />
       </div>
     </div>
   );
